7d2801f82897b5a6913118e1b3a137292c7de744
SMLFRD20 <tomato4ever08@gmail.com>
2025-04-01 22:58:49 -0400

    Add files via upload

---
+++ (Sergio Lugo 24-0951) Crear Base de Datos de Centralita -2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Llamada
+{
+    public string NumeroOrigen { get; set; }
+    public string NumeroDestino { get; set; }
+    public int Duracion { get; set; }
+    public double Costo { get; set; }
+
+    public Llamada(string numeroOrigen, string numeroDestino, int duracion, double costo)
+    {
+        NumeroOrigen = numeroOrigen;
+        NumeroDestino = numeroDestino;
+        Duracion = duracion;
+        Costo = costo * 63;
+    }
+
+    public void MostrarInfo()
+    {
+        Console.WriteLine($"Origen: {NumeroOrigen} - Destino: {NumeroDestino} - Duración: {Duracion} segundos - Costo: {Costo:N2} DOP");
+    }
+}
+
+class Centralita
+{
+    private List<Llamada> registros = new List<Llamada>();
+
+    public void RegistrarLlamada(string origen, string destino, int duracion, double costo)
+    {
+        registros.Add(new Llamada(origen, destino, duracion, costo));
+        Console.WriteLine("Llamada registrada con éxito.");
+    }
+
+    public void MostrarRegistros()
+    {
+        Console.WriteLine("\nRegistros de llamadas:");
+        foreach (var llamada in registros)
+        {
+            llamada.MostrarInfo();
+        }
+    }
+}
+
+class Programa
+{
+    static void Main()
+    {
+        Centralita centralita = new Centralita();
+        centralita.RegistrarLlamada("809-555-1234", "829-555-5678", 120, 15.75);
+        centralita.RegistrarLlamada("849-777-4321", "809-333-7890", 300, 45.50);
+        centralita.MostrarRegistros();
+    }
+}
